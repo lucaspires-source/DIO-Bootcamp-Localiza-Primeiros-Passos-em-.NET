e5135ca3ab420fc71c00a19fde03bda9c1c4ced1
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-03-01 14:15:52 -0300

    primeiro commit

---
+++ EstruturadoPrograma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EstruturadoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
